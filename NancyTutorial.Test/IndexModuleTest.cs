f153c14c7dfab91b4c11e931178b0cc7098b6792
Luckylv <alwaysbest@qq.com>
2016-07-07 16:59:26 +0800

    test infrastructure

---
+++ NancyTutorial.Test/IndexModuleTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+namespace NancyTutorial.Test
+{
+    [TestFixture]
+    public class IndexModuleTest
+    {
+        [Test]
+        public void root_should_return_response_ok()
+        {
+            var bootstrapper = new ConfigurableBootstrapper(with =>
+            {
+                with.Module<IndexModule>();
+                with.RootPathProvider(new TestRootPathProvider());
+            });
+            var browser = new Browser(bootstrapper);
+            var response = browser.Get("/");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
